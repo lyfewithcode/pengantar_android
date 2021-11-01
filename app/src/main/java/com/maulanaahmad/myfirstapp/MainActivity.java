daa54642a376c794a8edbc11bef4b9540076fb05
lyfewithcode <lyfewithcode@gmail.com>
2021-11-01 23:47:27 +0700

    Activity Lifecycle

--- app/src/main/java/com/maulanaahmad/myfirstapp/MainActivity.java
+++ app/src/main/java/com/maulanaahmad/myfirstapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.maulanaahmad.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("tes_halo_dunia", "onCreate: Activity dibuat");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("tes_halo_dunia", "onStart: Activity dimulai");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("tes_halo_dunia", "onPause: Activity dihentikan sementara");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.i("tes_halo_dunia", "onPostResume: Activity dilanjutkan kembali");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("tes_halo_dunia", "onStop: Activity dihentikan");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("tes_halo_dunia", "onDestroy: Activity dihancurkan");
     }
 }
